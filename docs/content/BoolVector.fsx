c2ca21a80d85a68a4da496e94249e9fdd123af6e
pbbwfc <phil.brooks@asr-services.com>
2015-08-26 16:52:20 +0100

    Added BoolVector document and expanded tests
    Added BoolVector.fsx and expanded the tests of BoolVector constructors

---
+++ docs/content/BoolVector.fsx
@@ -0,0 +1,65 @@
+﻿(*** hide ***)
+// This block of code is omitted in the generated HTML documentation. Use 
+// it to define helpers that you do not want to show in the documentation.
+#I "../../bin/FCore"
+(**
+BoolVector
+============
+FCore provides specialised vectors that hold boolean values. The primary use of these is for linear algebra. 
+Creating BoolVectors
+--------------------
+There are a number of options provided to create these vectors.
+
+They can be created by defining then length using an integer or long and the value to be used for each element, or if you just provide a value you will get a vector of length 1: 
+*)
+#r "FCore.dll"
+
+open FCore
+
+let v1 = new BoolVector(5, true)
+let v2 = new BoolVector(3L, false)
+let v3 = new BoolVector(false)
+(** This gives a value for v2 of: *)
+(*** include-value: v2 ***)
+(**
+You can also create a vector by just providing a sequence of bool values or by specifiying a length and an initiializer function:
+*)
+let v4 = new BoolVector([ false; true ])
+
+let v5 = new BoolVector(3, fun i -> 
+                       if i = 1 then false
+                       else true)
+(** This gives a value for v5 of: *)
+(*** include-value: v5 ***)
+(**
+You can also create a vector from an array of bools. For performance reasons you can specify whether to copy the data or just hold a reference to the data.
+*)
+let v6 = new BoolVector([| false; true |], true)
+//v7 just holds a reference to the provided array
+let v7 = new BoolVector([| false; true |], false)
+
+(**
+You can also create a vector by converting a single bool or an array of bools using !! operator from the ExplicitConversion module.
+*)
+open FCore.ExplicitConversion
+let v8 : BoolVector = !!false
+let v9 : BoolVector = !![| false; true |]
+(**
+Operators
+---------
+There are a full range of operators provided for working with BoolVectors. These include:
+
+* **==** - tests whether two vectors are equal 
+* **!=** - tests whether two vectors are different 
+* **.&&** - creates a new vector which is the results of applying && to each element of the two vectors  
+* **.||** - creates a new vector which is the results of applying || to each element of the two vectors  
+
+Here are some examples:
+*)
+v1 == v2 //false
+v1 != v2 //true
+let v10 = new BoolVector([ false; true ; false ])
+let v11 = new BoolVector([ false; true ; true ])
+let b1 = v10 .&& v11 //?? should be false, true,false but is false,false,false???
+let b2 = v10 .|| v11 //false,true,true
+
